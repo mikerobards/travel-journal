0631c1c6b9b5bb2a12d51a3d3751f4daff09d3f0
Michael Robards <mikerobards@icloud.com>
2021-12-28 13:11:27 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className='navbar'>
+            <i class="fas fa-globe-americas"></i>
+            <p>my travel journal.</p>
+        </nav>
+    )
+}
